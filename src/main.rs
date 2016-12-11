11e2f464eb9e777b2cfc8134d24d28694af33fc2
kud1ing <kud1ing@users.noreply.github.com>
2016-12-11 18:59:53 +0100

    use sfackler's hints

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
 extern crate jni_sys;
 
 use jni_sys::{JavaVM, JavaVMInitArgs, JavaVMOption, JNI_FALSE, JNI_VERSION_1_8, JNIInvokeInterface_};
-use std::ffi::CString;
+// use std::ffi::CString;
 use std::os::raw::c_void;
+use std::ptr;
 
 
 #[link(name="jvm")]
 extern {
-    fn JNI_CreateJavaVM(pvm: *mut *mut JavaVM, penv: *mut *mut c_void, args: *mut c_void);
+    fn JNI_CreateJavaVM(pvm: *mut *mut JavaVM, penv: *mut *mut c_void, args: *mut JavaVMInitArgs);
 }
 
 fn main() {
@@ -15,17 +16,19 @@ fn main() {
     let mut jvm_options = [JavaVMOption::default()];
     // jvm_options[0].optionString = CString::new("-Djava.class.path=/usr/lib/java").unwrap().into_raw();
 
-    let mut jvm_arguments = [JavaVMInitArgs::default()];
-    jvm_arguments[0].version = JNI_VERSION_1_8;
-    jvm_arguments[0].nOptions = 1;
-    jvm_arguments[0].options = jvm_options.as_mut_ptr();
-    jvm_arguments[0].ignoreUnrecognized = JNI_FALSE;
+    let mut jvm_arguments = JavaVMInitArgs::default();
+    jvm_arguments.version = JNI_VERSION_1_8;
+    jvm_arguments.nOptions = 1;
+    jvm_arguments.options = jvm_options.as_mut_ptr();
+    jvm_arguments.ignoreUnrecognized = JNI_FALSE;
 
-    let mut jvm = [JNIInvokeInterface_::default()];
-    let mut env = [JNIInvokeInterface_::default()];
+    let mut jvm = ptr::null_mut();
+    let mut env = ptr::null_mut();
 
     // TODO: make this compile
-    JNI_CreateJavaVM(jvm.as_mut_ptr(), env.as_mut_ptr(), jvm_arguments.as_mut_ptr() as *mut c_void);
+    unsafe {
+        let ret = JNI_CreateJavaVM(&mut jvm, &mut env as *mut _, &mut jvm_arguments as *mut _);
+    }
 
     // TODO
     /*
