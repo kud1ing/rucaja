759ddac7b7b46776b8c320487d4715a702931258
kud1ing <kud1ing@users.noreply.github.com>
2016-12-11 23:44:51 +0100

    destroy the JVM. document the direct translation of the jni.h function signature

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,13 @@ extern crate jni_sys;
 
 use jni_sys::{JavaVM, JavaVMInitArgs, jclass, jint, jmethodID, JNI_FALSE, JNI_VERSION_1_8, JNIEnv};
 use std::ffi::CString;
+use std::os::raw::c_void;
 use std::ptr;
 
 
 #[link(name="jvm")]
 extern {
+    //fn JNI_CreateJavaVM(pvm: *mut *mut JavaVM, penv: *mut *mut c_void, args: *mut c_void) -> jint;
     fn JNI_CreateJavaVM(pvm: *mut *mut JavaVM, penv: *mut *mut JNIEnv, args: *mut JavaVMInitArgs) -> jint;
 }
 
@@ -30,16 +32,16 @@ fn main() {
         // Get the Java class.
         let java_class : jclass = (**env).FindClass.unwrap()(env, java_class_name.as_ptr());
 
-        // Get the Java method ID.
+        // Get the Java static method ID.
         let java_method_id : jmethodID = (**env).GetStaticMethodID.unwrap()(
             env, java_class, java_method_name.as_ptr(), java_method_signature.as_ptr()
         );
 
-        // Call the Java method.
+        // Call the Java static method.
         (**env).CallStaticVoidMethod.unwrap()(env, java_class, java_method_id);
 
-        // TODO
-        //jvm->DestroyJavaVM();
+        // Destroy the JVM.
+        (**jvm).DestroyJavaVM.unwrap()(jvm);
     }
 
 }
