0501010542bb752e387d41be2f57368c28351ff0
kud1ing <kud1ing@users.noreply.github.com>
2016-12-12 00:00:20 +0100

    refactor

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,38 @@ extern crate jni_sys;
 
 use jni_sys::{JavaVM, JavaVMInitArgs, jclass, jint, jmethodID, JNI_FALSE, JNI_VERSION_1_8, JNIEnv};
 use std::ffi::CString;
-use std::os::raw::c_void;
 use std::ptr;
+
+
+struct Jvm {
+    jvm: *mut JavaVM,
+    jni_environment: *mut JNIEnv,
+}
+
+impl Jvm {
+
+    ///
+    fn new() -> Jvm {
+
+        let mut jvm = Jvm {
+            jvm: ptr::null_mut(),
+            jni_environment: ptr::null_mut(),
+        };
+
+        let mut jvm_arguments = JavaVMInitArgs::default();
+        jvm_arguments.version = JNI_VERSION_1_8;
+        jvm_arguments.nOptions = 0;
+        jvm_arguments.ignoreUnrecognized = JNI_FALSE;
+
+        unsafe {
+            let _ = JNI_CreateJavaVM(
+                &mut jvm.jvm, &mut jvm.jni_environment as *mut _, &mut jvm_arguments as *mut _
+            );
+        }
+
+        jvm
+    }
+}
 
 
 #[link(name="jvm")]
@@ -18,30 +48,26 @@ fn main() {
     let java_method_name : CString = CString::new("helloRust").unwrap();
     let java_method_signature : CString = CString::new("()V").unwrap();
 
-    let mut jvm_arguments = JavaVMInitArgs::default();
-    jvm_arguments.version = JNI_VERSION_1_8;
-    jvm_arguments.nOptions = 0;
-    jvm_arguments.ignoreUnrecognized = JNI_FALSE;
-
-    let mut jvm : *mut JavaVM = ptr::null_mut();
-    let mut env : *mut JNIEnv = ptr::null_mut();
+    let jvm = Jvm::new();
 
     unsafe {
-        let _ = JNI_CreateJavaVM(&mut jvm, &mut env as *mut _, &mut jvm_arguments as *mut _);
-
         // Get the Java class.
-        let java_class : jclass = (**env).FindClass.unwrap()(env, java_class_name.as_ptr());
-
-        // Get the Java static method ID.
-        let java_method_id : jmethodID = (**env).GetStaticMethodID.unwrap()(
-            env, java_class, java_method_name.as_ptr(), java_method_signature.as_ptr()
+        let java_class : jclass = (**jvm.jni_environment).FindClass.unwrap()(
+            jvm.jni_environment, java_class_name.as_ptr()
         );
 
-        // Call the Java static method.
-        (**env).CallStaticVoidMethod.unwrap()(env, java_class, java_method_id);
+        // Get the Java method ID.
+        let java_method_id : jmethodID = (**jvm.jni_environment).GetStaticMethodID.unwrap()(
+            jvm.jni_environment, java_class, java_method_name.as_ptr(), java_method_signature.as_ptr()
+        );
+
+        // Call the Java static void method.
+        (**jvm.jni_environment).CallStaticVoidMethod.unwrap()(
+            jvm.jni_environment, java_class, java_method_id
+        );
 
         // Destroy the JVM.
-        (**jvm).DestroyJavaVM.unwrap()(jvm);
+        (**jvm.jvm).DestroyJavaVM.unwrap()(jvm.jvm);
     }
 
 }
