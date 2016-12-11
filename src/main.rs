c1b070d6faee0e90f4e97ee435d4ddd63a45b82f
kud1ing <kud1ing@users.noreply.github.com>
2016-12-11 20:07:05 +0100

    try to find the Java class

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
 extern crate jni_sys;
 
-use jni_sys::{JavaVM, JavaVMInitArgs, JavaVMOption, jint, JNI_FALSE, JNI_VERSION_1_8};
+use jni_sys::{JavaVM, JavaVMInitArgs, JavaVMOption, jclass, jint, JNI_FALSE, JNI_VERSION_1_8, JNIEnv, JNINativeInterface_};
 // use std::ffi::CString;
+use std::os::raw::c_char;
 use std::os::raw::c_void;
 use std::ptr;
 
 
 #[link(name="jvm")]
 extern {
-    fn JNI_CreateJavaVM(pvm: *mut *mut JavaVM, penv: *mut *mut c_void, args: *mut JavaVMInitArgs) -> jint;
+    fn JNI_CreateJavaVM(pvm: *mut *mut JavaVM, penv: *mut *mut JNIEnv, args: *mut JavaVMInitArgs) -> jint;
 }
 
 fn main() {
@@ -22,12 +23,15 @@ fn main() {
     //jvm_arguments.options = jvm_options.as_mut_ptr();
     jvm_arguments.ignoreUnrecognized = JNI_FALSE;
 
-    let mut jvm = ptr::null_mut();
-    let mut env = ptr::null_mut();
+    let mut jvm : *mut JavaVM = ptr::null_mut();
+    let mut env : *mut JNIEnv = ptr::null_mut();
 
     unsafe {
         let _ = JNI_CreateJavaVM(&mut jvm, &mut env as *mut _, &mut jvm_arguments as *mut _);
     }
+
+    // TODO: make this compile
+    let java_class = (*env).FindClass.unwrap()("Test");
 
     // TODO
     /*
