04a61ffd351e332d048420c1e98928c2cdb1d98f
kud1ing <kud1ing@users.noreply.github.com>
2016-12-13 20:43:55 +0100

    search JAVA_HOME

--- build.rs
+++ build.rs
@@ -1,3 +1,13 @@
+use std::env;
+
 fn main() {
-    println!("cargo:rustc-link-search=native=/Library/Java/JavaVirtualMachines/jdk1.8.0_51.jdk/Contents/Home/jre/lib/server");
+
+    // Try to determine the Java home directory so that we can linl to `libjvm`.
+    let java_home =
+        env::var("JAVA_HOME")
+        .ok()
+        .unwrap_or("/Library/Java/JavaVirtualMachines/jdk1.8.0_51.jdk/Contents/Home".to_string());
+
+    print!("cargo:rustc-link-search=native=");
+    println!("{}/jdk1.8.0_51.jdk/Contents/Home/jre/lib/server", java_home);
 }
