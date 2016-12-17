2cf78d14248e6707ef0c54856cfd24d9e897cea8
kud1ing <kud1ing@users.noreply.github.com>
2016-12-17 21:04:13 +0100

    add static methods

--- examples/Test.java
+++ examples/Test.java
@@ -1,7 +1,38 @@
 class Test {
+    public static boolean staticBooleanMethod() {
+        return true;
+    }
+
+    public static byte staticByteMethod() {
+        return 42;
+    }
+
+    public static char staticCharMethod() {
+        return 'a';
+    }
+
+    public static double staticDoubleMethod() {
+        return 42.42f;
+    }
+
+    public static float staticFloatMethod() {
+        return 42.1f;
+    }
+
+    public static int staticIntMethod() {
+        return 42;
+    }
+
+    public static long staticLongMethod() {
+        return 4242L;
+    }
+
+    public static String staticObjectMethod() {
+        return "Foo";
+    }
 
     public static void staticVoidMethod() {
-        System.out.println("Hello Rust!");
+        System.out.println("staticVoidMethod()");
     }
 
     public static void main(String [] args) {
--- examples/main.rs
+++ examples/main.rs
@@ -13,7 +13,7 @@ fn main() {
     let jvm = Jvm::new(&jvm_options);
 
     let jvm_class = jvm.get_class("Test").expect("Could not find JVM class");
-    let jvm_method = jvm.get_static_method(&jvm_class, "staticVoidMethod", "()V").expect("Could not find JVM method");
 
+    let jvm_method = jvm.get_static_method(&jvm_class, "staticVoidMethod", "()V").expect("Could not find JVM method");
     jvm.call_static_void_method(&jvm_class, &jvm_method);
 }
