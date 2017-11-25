f64314a2f1a91fbcfe9ddbc9edc87a5f384000de
kud1ing <kud1ing@users.noreply.github.com>
2017-11-25 08:38:43 +0100

    small adjustments [NFC]

--- src/jvm.rs
+++ src/jvm.rs
@@ -131,16 +131,16 @@ impl Jvm {
     /// ```
     pub fn new(jvm_option_strings: &[&str]) -> Jvm {
 
-        // Wrap the JVM option string slices in a vector of `CString`s.
         let mut jvm_option_cstrings: Vec<CString> = Vec::new();
 
+        // Wrap the JVM option string slices in a vector of `CString`s.
         for jvm_option_string in jvm_option_strings {
             jvm_option_cstrings.push(CString::new(*jvm_option_string).unwrap());
         }
 
-        // Create a vector of `JavaVMOption`s, each referencing a `CString`.
         let mut jvm_options: Vec<JavaVMOption> = Vec::new();
 
+        // Create a vector of `JavaVMOption`s, each referencing a `CString`.
         for jvm_option_cstring in &jvm_option_cstrings {
             let jvm_option = JavaVMOption {
                 optionString: jvm_option_cstring.as_ptr() as *mut i8,
--- src/jvm_type_signature.rs
+++ src/jvm_type_signature.rs
@@ -11,16 +11,21 @@ pub enum JvmType {
     Long,
     Float,
     Double,
-    /// On the left is the package componenets, on the right is the class name itself.
+
+    /// On the left is the package components, on the right is the class name itself.
     Class(Vec<String>, String),
+
     /// Makes it an array of the type.
     Array(Box<JvmType>) // Boxed because sizing is silly.
 }
 
 // TODO: `Into<String>` or `ToString`?
 impl Into<String> for JvmType {
+
     fn into(self) -> String {
+
         use self::JvmType::*;
+
         match self {
             Boolean => "Z".into(),
             Byte => "B".into(),
@@ -51,14 +56,15 @@ impl Into<String> for JvmType {
 pub fn compute_jvm_method_signature(ret: Option<JvmType>, args: Vec<JvmType>) -> String {
 
     let mut a = String::new();
+
     for arg in args {
         let rep: String = arg.into(); // Still complaining about type annotations.
         a.push_str(rep.as_str());
     }
 
     let rrep: String = ret.map_or("".into(), |t| t.into());
-    format!("({}){}", a, rrep)
 
+    format!("({}){}", a, rrep)
 }
 
 // TODO Write a macro that wraps the call to compute_jvm_method_signature, because it's still ugly.
@@ -70,22 +76,24 @@ mod tests {
     use super::compute_jvm_method_signature;
 
     #[test]
-    fn jvm_method_test_1() {
-        // void foo();
-        assert_eq!(compute_jvm_method_signature(None, vec![]), String::from("()"))
-    }
+    fn test_jvm_methods() {
 
-    #[test]
-    fn jvm_method_test_2() {
-        // long f (int n, String s, int[] arr); // (from the JNI docs)
+        // `void foo()`
+        assert_eq!(
+            compute_jvm_method_signature(None, vec![]),
+            String::from("()")
+        );
+
+        // `long f(int n, String s, int[] arr)`
         assert_eq!(
             compute_jvm_method_signature(
                 Some(JvmType::Long),
                 vec![
                     JvmType::Int,
                     JvmType::Class(vec!["java".into(), "lang".into()], "String".into()),
-                    JvmType::Array(Box::new(JvmType::Int))]),
-            String::from("(ILjava/lang/String;[I)J"))
+                    JvmType::Array(Box::new(JvmType::Int))]
+            ),
+            String::from("(ILjava/lang/String;[I)J")
+        );
     }
-
 }
